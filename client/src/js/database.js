79cf070950491b281428c0c3b6c19148148566f0
Mariah <110792371+peachysnicker@users.noreply.github.com>
2023-03-18 16:49:58 -0700

    database.js complete

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,39 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Accept content and PUT it to the database
+export const putDb = async (content) => {
+  console.log("PUT to the database");
+  // Create a connection to database with version wanted (1)
+  const jateDb = await openDB("jate", 1);
+  const transaction = jateDb.transaction("jate", "readwrite");
+  const store = transaction.objectStore("jate");
+  const request = store.put({ text: content });
+  const result = await request;
+  console.log("Data successfully saved to database", result);
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// GET all the content from the database
+export const getDb = async () => {
+  console.log("GET from the database");
+  const jateDb = await openDB("jate", 1);
+  const transaction = jateDb.transaction("jate", "readonly");
+  const store = transaction.objectStore("jate");
+  const request = store.getAll();
+  const result = await request;
+  console.log("result.value", result);
+  return result.value;
+};
 
 initdb();
